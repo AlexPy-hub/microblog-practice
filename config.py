3a2e388d6fbf867f8949e5acc9616b4f28e8c3ad
San1xe <narkoznartov@mail.ru>
2026-09-19 21:34:44 +0300

    Create SQLite instance directory

--- config.py
+++ config.py
@@ -2,12 +2,14 @@ from pathlib import Path
 import os
 
 BASE_DIR = Path(__file__).resolve().parent
+INSTANCE_DIR = BASE_DIR / "instance"
+INSTANCE_DIR.mkdir(parents=True, exist_ok=True)
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key")
 
     SQLALCHEMY_DATABASE_URI = (
-        f"sqlite:///{BASE_DIR / 'instance' / 'microblog.db'}"
+        f"sqlite:///{INSTANCE_DIR / 'microblog.db'}"
     )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
